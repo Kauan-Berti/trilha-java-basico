41c7919fe7cebab09f90cf12bfd498fb809f0685
Kauan-Berti <kauan.g.berti@gmail.com>
2025-12-12 23:13:28 -0300

    feat: adicionando exception para validar a entrada de dados

--- desafios/controle-fluxo/DesafioControleFluxo/src/Contador.java
+++ desafios/controle-fluxo/DesafioControleFluxo/src/Contador.java
@@ -1,17 +1,20 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
 
-        System.out.println("Digite o primeiro parâmetro");
-        int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro");
-        int parametroDois = terminal.nextInt();
-
         try {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
             contar(parametroUm, parametroDois);
 
+        } catch (InputMismatchException e) {
+            System.out.println("Parametro invalido, digite um valor inteiro, Ex: 1");
+            
         } catch (ParametrosInvalidosException e) {
             System.out.println("O valor informado no primeiro parâmetro deve ser menor que o segundo!");
         }
